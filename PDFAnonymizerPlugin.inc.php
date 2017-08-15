a23aad5ecc946808ee671b110a16d698bff7bda2
angusiguess <ethdem@gmail.com>
2017-08-15 16:11:38 -0300

    Complete metadata removal and linearization

--- PDFAnonymizerPlugin.inc.php
+++ PDFAnonymizerPlugin.inc.php
@@ -9,6 +9,7 @@
  *
  * @package plugins.generic.pdfAnonymizer
  * @class PDFAnonymizerPlugin
+ * @ingroup plugins
  * @brief Plugin that will remove identifying information from PDFs on upload
  */
 
@@ -18,6 +19,9 @@ class PDFAnonymizerPlugin extends GenericPlugin {
 
 		function register($category, $path) {
 				$success = parent::register($category, $path);
+				if($success) {
+						HookRegistry::register('FileManager::uploadFileFinished', array($this, 'fileHandlerCallback'));
+				}
 				return $success;
 		}
 
@@ -31,6 +35,54 @@ class PDFAnonymizerPlugin extends GenericPlugin {
 
 		function getDescription() {
 				return __('plugins.generic.pdfAnonymizer.description');
+		}
+
+
+		/**
+		 * Scrub EXIF Data from a file by filename
+		 * @param $fileName string
+		 * @return boolean
+		 */
+		function scrubExif($fileName) {
+				// Remove exif metadata
+				$output = "";
+				$exifSuccess = 0;
+				exec("exiftool -all:all= " . $fileName,
+						 $output,
+						 $exifSuccess);
+				$linearizeSuccess = 0;
+				if($exifSuccess == 0) {
+						unlink($fileName . "_original");
+						$tempFilePath = $fileName . "tmp";
+						// Linearizing the pdf removes any pre-exif metadata.
+						exec("qpdf --linearize " . $fileName . " " . $tempFilePath,
+								 $output,
+								 $linearizeSuccess);
+						if($linearizeSuccess == 0) {
+								unlink($fileName);
+								rename($tempFilePath, $fileName);
+						}
+				}
+
+			  return $exifSuccess && ($linearizeSuccess == 0);
+		}
+
+		/**
+		 * File upload hook that scrubs metadata from PDF submissions as they're uploaded.
+		 * @param $hookName string
+		 * @param $args array
+		 * @return boolean
+		 */
+		function fileHandlerCallback($hookName, $args) {
+				$fileName = $args[0];
+				$destFileName = $args[1];
+				$fileType = $args[2];
+				$returnValue = $args[3];
+
+				if ($fileType === 'pdf') {
+						$returnValue = $this->scrubExif($destFileName);
+				}
+				return $returnValue;
 		}
 
 
